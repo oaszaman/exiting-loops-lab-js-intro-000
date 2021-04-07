e513286aae7612fc5193904054f6c84ea5d3bdc1
Shahzab Zaman <o.aszaman00@gmail.com>
2021-04-07 23:58:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 function breakOut(array, changeValue, stopValue) {
-
+   for (var i = 0; l = array.length; i < i; i++){
+     if (array[i] === stopValue) {
+       break;
+     }
+     array[i] = changeValue;
+   }
+   return array
 }
 
 function keepGoing(array, changeValue, skipValue) {
-
+  
 }
 
 function findBy(array, findFn) {
-
+  
 }
